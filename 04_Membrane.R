034065d935b0028c31413c874cf4bfd8628cef1a
Benben1104 <1487383460@qq.com>
2025-03-25 17:29:18 +0800

    Update

--- 04_Membrane.R
+++ 04_Membrane.R
@@ -1,21 +1,79 @@
+library(RColorBrewer)
+library(ggplot2)
+library(ggalt)
+
+d_CEF_1 <-  read.csv("CEF_potential.csv")
+head(d_CEF_1)
+
+ggplot(d_CEF_1, aes(x=Time, y=Mean, color=Surfactant))+
+  theme_bw()+
+  scale_y_continuous(limits = c(0.5, 1.3))+
+  geom_point(size=4, alpha=0.8)+
+  geom_errorbar(aes(ymin = Mean-SD, ymax = Mean+SD), width=0)+
+  geom_xspline(spline_shape = 1.0)+
+  theme(strip.text.x = element_text(size=16))+
+  labs(x=expression("Time (min)"),
+       y=expression("Fold change of potential"),
+       title = expression("CEF"),
+       color=NULL)+
+  scale_color_manual(values = c("Ctrl"="#B3D9CE", "DTAC"="#C24E44", "SDS"="#2A5196", "TX-100"="#7367BE"),
+                     breaks = c("Ctrl", "DTAC", "SDS", "TX-100"))+
+  theme(axis.text.x=element_text(size = 18, face = "bold", vjust = 0))+
+  theme(axis.text.y = element_text(size = 18, face = "bold"))+
+  theme(axis.title.x = element_text(size=20, face = "bold", vjust = -0.5))+
+  theme(axis.title.y = element_text(size=20, face = "bold", vjust = 2))+
+  theme(title = element_text(size = 16, face = "bold"))+
+  theme(legend.text = element_text(size=16, face = "bold"))+
+  theme(panel.border = element_rect(fill=NA,color="black", size=2, linetype="solid"))
+
+ggsave("CEF_potential.png", width=600/90, height=390/90, dpi=600, unit="in")
+
+
+
+d_TET_1 <- read.csv("TET_potential.csv")
+head(d_TET_1)
+
+ggplot(d_TET_1, aes(x=Time, y=Mean, color=Surfactant))+
+  theme_bw()+
+  scale_y_continuous(limits = c(0.5, 1.3))+
+  geom_point(size=4, alpha=0.8)+
+  geom_errorbar(aes(ymin = Mean-SD, ymax = Mean+SD), width=0)+
+  geom_xspline(spline_shape = 1.0)+
+  theme(strip.text.x = element_text(size=16))+
+  labs(x=expression("Time (min)"),
+       y=expression("Fold change of potential"),
+       title = expression("TET"),
+       color=NULL)+
+  scale_color_manual(values = c("Ctrl"="#B3D9CE", "DTAC"="#C24E44", "SDS"="#2A5196", "TX-100"="#7367BE"),
+                     breaks = c("Ctrl", "DTAC", "SDS", "TX-100"))+
+  theme(axis.text.x=element_text(size = 18, face = "bold", vjust = 0))+
+  theme(axis.text.y = element_text(size = 18, face = "bold"))+
+  theme(axis.title.x = element_text(size=20, face = "bold", vjust = -0.5))+
+  theme(axis.title.y = element_text(size=20, face = "bold", vjust = 2))+
+  theme(title = element_text(size = 16, face = "bold"))+
+  theme(legend.text = element_text(size=16, face = "bold"))+
+  theme(panel.border = element_rect(fill=NA,color="black", size=2, linetype="solid"))
+
+ggsave("TET_potential.png", width=600/90, height=390/90, dpi=600, unit="in")
+
+
+
 library(sf)
 library(tidyverse)
-library(ggplot2)
-library(RColorBrewer)
 library(ggpubr)
 library(scales)
 
-d_CEF <- read.csv("CEF_membrane.csv")
-head(d_CEF)
+d_CEF_2 <- read.csv("CEF_membrane.csv")
+head(d_CEF_2)
 
-residuals <- (aov(Membrane.permeability ~ Surfactant, data=d_CEF))$residuals
+residuals <- (aov(Membrane.permeability ~ Surfactant, data=d_CEF_2))$residuals
 shapiro.test(residuals)
-bartlett.test(Membrane.permeability ~ Surfactant, data=d_CEF)
-aov.1 <- aov(Membrane.permeability ~ Surfactant, data=d_CEF)
+bartlett.test(Membrane.permeability ~ Surfactant, data=d_CEF_2)
+aov.1 <- aov(Membrane.permeability ~ Surfactant, data=d_CEF_2)
 summary(aov.1)
 TukeyHSD(aov.1)
 
-ggplot(d_CEF, aes(Surfactant, Membrane.permeability, fill= Surfactant))+
+ggplot(d_CEF_2, aes(Surfactant, Membrane.permeability, fill= Surfactant))+
   theme_bw()+
   scale_y_continuous(limits = c(0, 1.5))+
   geom_jitter(aes(Surfactant, Membrane.permeability), width =0.22, 
@@ -41,14 +99,14 @@ ggsave("CEF_permeability.png", width=565/90, height=360/90, dpi=600, unit="in")
 
 
 
-residuals <- (aov(Membrane.hydrophobicity ~ Surfactant, data=d_CEF))$residuals
+residuals <- (aov(Membrane.hydrophobicity ~ Surfactant, data=d_CEF_2))$residuals
 shapiro.test(residuals)
-bartlett.test(Membrane.hydrophobicity ~ Surfactant, data=d_CEF)
-aov.2 <- aov(Membrane.hydrophobicity ~ Surfactant, data=d_CEF)
+bartlett.test(Membrane.hydrophobicity ~ Surfactant, data=d_CEF_2)
+aov.2 <- aov(Membrane.hydrophobicity ~ Surfactant, data=d_CEF_2)
 summary(aov.2)
 TukeyHSD(aov.2)
 
-ggplot(d_CEF, aes(Surfactant, Membrane.hydrophobicity, fill= Surfactant))+
+ggplot(d_CEF_2, aes(Surfactant, Membrane.hydrophobicity, fill= Surfactant))+
   theme_bw()+
   scale_y_continuous(limits = c(0, 1.25))+
   geom_jitter(aes(Surfactant, Membrane.hydrophobicity), width =0.22, 
@@ -74,17 +132,17 @@ ggsave("CEF_hydrophobicity.png", width=565/90, height=360/90, dpi=600, unit="in"
 
 
 
-d_TET <- read.csv("TET_membrane.csv")
-head(d_TET)
+d_TET_2 <- read.csv("TET_membrane.csv")
+head(d_TET_2)
 
-residuals <- (aov(Membrane.permeability ~ Surfactant, data=d_TET))$residuals
+residuals <- (aov(Membrane.permeability ~ Surfactant, data=d_TET_2))$residuals
 shapiro.test(residuals)
-bartlett.test(Membrane.permeability ~ Surfactant, data=d_TET)
-aov.3 <- aov(Membrane.permeability ~ Surfactant, data=d_TET)
+bartlett.test(Membrane.permeability ~ Surfactant, data=d_TET_2)
+aov.3 <- aov(Membrane.permeability ~ Surfactant, data=d_TET_2)
 summary(aov.3)
 TukeyHSD(aov.3)
 
-ggplot(d_TET, aes(Surfactant, Membrane.permeability, fill= Surfactant))+
+ggplot(d_TET_2, aes(Surfactant, Membrane.permeability, fill= Surfactant))+
   theme_bw()+
   scale_y_continuous(limits = c(0, 3), labels = number_format(accuracy = 0.1))+
   geom_jitter(aes(Surfactant, Membrane.permeability), width =0.22, 
@@ -110,14 +168,14 @@ ggsave("TET_permeability.png", width=565/90, height=360/90, dpi=600, unit="in")
 
 
 
-residuals <- (aov(Membrane.hydrophobicity ~ Surfactant, data=d_TET))$residuals
+residuals <- (aov(Membrane.hydrophobicity ~ Surfactant, data=d_TET_2))$residuals
 shapiro.test(residuals)
-bartlett.test(Membrane.hydrophobicity ~ Surfactant, data=d_TET)
-aov.4 <- aov(Membrane.hydrophobicity ~ Surfactant, data=d_TET)
+bartlett.test(Membrane.hydrophobicity ~ Surfactant, data=d_TET_2)
+aov.4 <- aov(Membrane.hydrophobicity ~ Surfactant, data=d_TET_2)
 summary(aov.4)
 TukeyHSD(aov.4)
 
-ggplot(d_TET, aes(Surfactant, Membrane.hydrophobicity, fill= Surfactant))+
+ggplot(d_TET_2, aes(Surfactant, Membrane.hydrophobicity, fill= Surfactant))+
   theme_bw()+
   scale_y_continuous(limits = c(0, 1.25))+
   geom_jitter(aes(Surfactant, Membrane.hydrophobicity), width =0.22, 
